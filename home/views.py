04edad972ef1cc8585dd0edcb3ab9992cbebd1e4
tepong32 <tepong32@gmail.com>
2021-03-29 11:38:36 +0800

    still...

--- home/views.py
+++ home/views.py
@@ -13,13 +13,15 @@ def home(request):
 	user = User
 	user_list = User.objects.all()[:5]
 	announcements = Announcement.objects.all().order_by("-date_posted")
-	posts = Post.objects.all().order_by("-date_posted")[:5]
+	posts = Post.objects.all().order_by("-date_posted")[:10]
+	postCount = Post.objects.all().count()
 	context = {
 		'announcements': announcements,
 		'users': user_list,
 		'posts': posts,
 		'quotes': Quote.objects.all(),
-		"usercount": user.objects.count(),
+		'usercount': user.objects.count(),
+		'postCount': postCount,
 	}
 	# template_folder/html_file
 	return render(request, 'home/index.html', context)
