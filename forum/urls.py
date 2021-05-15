0c62f7fd6cc63f5df4b8de8f1b4638907cf60ed5
tepong32 <tepong32@gmail.com>
2021-05-15 12:38:31 +0800

    mods mods mods

--- forum/urls.py
+++ forum/urls.py
@@ -1,10 +1,7 @@
 
 from django.urls import path, include
 
-# from django documentation -- check CoreyMS' tutorial Part 8 / 22:30
-from . import views
-from django.conf import settings
-from django.conf.urls.static import static
+# from django documentation -- check CoreyMS' django tutorial Part 8 / 22:30
 from .views import (
     UserPostFilter,
     PostDetailView,
@@ -18,21 +15,27 @@ from .views import (
     CommentCreateView,
     PostCommentCreateView,
     )
+# alternatively, you can just use "from . import views".
+# however, importing views one-by-one seems to be a better option so you can remember which views you have already worked on.
 
 
 urlpatterns = [
-    path('', ForumHomeView.as_view(), name="forum-home"),             # home for blog app
+    path('', ForumHomeView.as_view(), name="forum-home"),             # homepage for blog app
     path('post/new/', PostCreateView.as_view(), name='post-new'),
-    path('post/detail/<int:pk>/add-comment/', PostCommentCreateView.as_view(), name='add-comment'),
-    # path('post/quick-create/', PostCreateViewModal.as_view(), name='quickpost'),
+    path('post/detail/<int:pk>/add-comment/', PostCommentCreateView.as_view(), name='add-comment'), # not working yet
     path('post/detail/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/detail/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('post/detail/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    # filters applied to posts
-    path('posts/<str:username>/', UserPostFilter.as_view(), name='user-posts'),
+    path('posts/<str:username>/', UserPostFilter.as_view(), name='user-posts'),     # filters applied to posts
     path('post/add-category/', CategoryCreateView.as_view(), name='add-category'),
     path('post/category/<str:cats>/', CategoryView, name='category'),
     path('post/detail/<int:pk>/like/', LikeView, name='like_post'),
     ]
 
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
